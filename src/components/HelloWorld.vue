9f8c9af4fc6a64a944017c9424f3a305b217fd89
Diego Garcia <diego.garcia220693@gmail.com>
2021-11-29 09:33:38 +0100

    se agrego funciones al git hut

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,39 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <div class="container">
+    {{nombre}}
+      <ul>
+        <li v-for="item in clientes" :key="item.id">
+            {{item.nombre}}
+        </li>
+      </ul>
+      <input type="text" v-model="nombre">
+      <br>
+      <a v-bind:href="url">{{url}}</a>
+      <p/>
+      <button @click='metodo1'>Ejecutar metodo</button>
+      <p></p>
+
+      <h1>Ejercicio 1</h1>
+      <p></p>
+      <input type="number" v-model="numero1">
+      <input type="number" v-model="numero2">
+      <p></p>
+      <button @click="sumar">Sumar +</button>
+      <p></p>
+      <h3>Resultado {{resultado}}</h3>
+
+      <p></p>
+      <h1>Ejercicio 2</h1>
+      <input type="text" v-model="tarea"/>
+     
+      <button @click="agregarTarea">Agregar tarea</button>
+      <h5>Listado de tareas</h5>
+       <ul>
+        <li v-for="item in tareas" :key="item">
+            {{item}}
+        </li>
+      </ul>
+  </div> 
 </template>
 
 <script>
@@ -35,24 +41,38 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data () {
+    return {
+      nombre : 'Alex',
+      mostrar : true,
+      clientes : [{id:1, nombre : 'Diego'},{id:2, nombre : 'Jonathan'}],
+      url : 'https://www.google.com',
+      numero1 : 0,
+      numero2 : 0,
+      resultado : 0,
+      tarea : null,
+      tareas : ['descripcion']
+    }
+  },
+  methods : {
+    metodo1 () {
+        alert("Click")
+    },
+    sumar () {
+      this.resultado = this.numero1+this.numero2
+    },
+    agregarTarea () {
+      alert("agregar tarea "+ this.tarea)
+      this.tareas.push(this.tarea)
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
 li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+  color: blue;
+  font-size: 34px;
 }
 </style>
